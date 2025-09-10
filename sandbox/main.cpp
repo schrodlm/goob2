3fcec8495709f59955ffa0aea050cc257999fd7e
Matěj Schrödl <matej.schrodl@gmail.com>
2025-09-10 19:53:32 +0200

    Add a vector module

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,6 +1,9 @@
 #include "goob.hpp"
+#include "vector.hpp"
 
 int main() {
     Goob goob = Goob();
     goob.hello();
+
+    Vector2<uint32_t> vec;
 }
---
+++ src/vector/vector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+template <typename T>
+struct Vector2{
+
+};
+
+template <typename T>
+struct Vector3{
+};
+
+// make strong types
