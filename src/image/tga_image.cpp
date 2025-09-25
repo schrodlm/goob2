e0974325b6844243a505d53c655c584993fcabc3
Matěj Schrödl <matej.schrodl@gmail.com>
2025-09-25 15:49:45 +0200

    Implement TGAImage constructors

--- src/image/tga_image.cpp
+++ src/image/tga_image.cpp
@@ -5,7 +5,17 @@
 
 #include "tga_image.hpp"
 
-using namespace tga;
+namespace tga {
+
+std::string error_to_string(TgaImageError error) {
+    switch(error) {
+        #define DEFINE_ERROR(name, value)   \
+            case TgaImageError::name:       \
+                return #name; 
+        #include "error_list.txt"
+#undef DEFINE_ERROR
+    }
+}
 
 TgaImage::TgaImage(uint16_t height, uint16_t width, RGBColor color) {
     header.width  = width;
@@ -13,4 +23,14 @@ TgaImage::TgaImage(uint16_t height, uint16_t width, RGBColor color) {
     memset(data.data(), color.to_code(), height * width);
 }
 
+TgaImage::TgaImage(TgaImageHeader header, std::span<uint8_t>& data_in)
+: header(header) {
+    data.assign(data_in.begin(), data_in.end());
+}
 
+TgaImage::TgaImage(TgaImageHeader header, RGBColor color /*= Colors::BLACK*/) : header(header) {
+    data.assign(header.width * header.height, color.to_code());
+}
+
+
+} // namespace tga
