6320496126c0565b14a6dd454b92ad58131519cf
Matěj Schrödl <matej.schrodl@gmail.com>
2025-10-11 22:34:59 +0200

    [TgaImage] Read TGAImage footer

--- src/image/tga_factory.cpp
+++ src/image/tga_factory.cpp
@@ -34,19 +34,45 @@ std::filesystem::path filepath) noexcept {
         return std::unexpected(TgaImageError::HEADER_READ_ERROR);
     }
 
+    // Check if footer exists
+    std::array<uint8_t, TGA_SIGNATURE_SIZE> possible_signature;
+    // Seek to 26 bytes before the end of file
+    file_stream.seekg(-static_cast<std::streamoff>(TGA_SIGNATURE_SIZE), std::ios::end);
+    // Read the last 26 bytes
+    file_stream.read(reinterpret_cast<char*>(possible_signature.data()), TGA_SIGNATURE_SIZE);
+    bool has_signature = (possible_signature == TGA_SIGNATURE);
+
+
+    // Seek back to after the header
+    file_stream.seekg(sizeof(TgaImageHeader), std::ios::beg);
     auto data_size = file_size - static_cast<std::streamsize>(sizeof(TgaImageHeader));
+    if(has_signature) {
+        data_size = data_size - static_cast<std::streamsize>(sizeof(TgaImageFooter));
+    }
     std::vector<uint8_t> data(data_size);
     file_stream.read(reinterpret_cast<char*>(data.data()), data_size);
-
     if(!file_stream || file_stream.gcount() != data_size) {
         return std::unexpected(TgaImageError::DATA_READ_ERROR);
     }
-
-    return create(header, data);
+    // Initialize footer
+    // TODO: Currently ignoring the developer and extension area
+    std::optional<TgaImageFooter> footer;
+    if(has_signature) {
+        TgaImageFooter temp_footer;
+        file_stream.read(reinterpret_cast<char*>(&temp_footer), sizeof(temp_footer));
+        if(!file_stream || file_stream.gcount() != sizeof(temp_footer)) {
+            return std::unexpected(TgaImageError::FOOTER_READ_ERROR);
+        }
+        footer = temp_footer;
+    } else {
+        footer = std::nullopt;
+    }
+    return create(header, data, footer);
 }
 
-std::expected<std::unique_ptr<TgaImage>, TgaImageError>
-create(TgaImageHeader header, std::span<uint8_t> data) noexcept {
+std::expected<std::unique_ptr<TgaImage>, TgaImageError> create(TgaImageHeader header,
+std::span<uint8_t> data,
+std::optional<TgaImageFooter> footer /*= get_default_tga_footer()*/) noexcept {
     TgaImageType type = static_cast<TgaImageType>(header.data_type_code);
     switch(type) {
     case TgaImageType::COLOR_MAPPED:
@@ -54,21 +80,21 @@ create(TgaImageHeader header, std::span<uint8_t> data) noexcept {
         if(!verify_header<TgaImageColorMapped>(header)) {
             return std::unexpected(TgaImageError::INVALID_HEADER);
         }
-        return std::make_unique<TgaImageColorMapped>(header, data);
+        return std::make_unique<TgaImageColorMapped>(header, data, footer);
 
     case TgaImageType::RGB:
     case TgaImageType::RLE_RGB:
         if(!verify_header<TgaImageRGB>(header)) {
             return std::unexpected(TgaImageError::INVALID_HEADER);
         }
-        return std::make_unique<TgaImageRGB>(header, data);
+        return std::make_unique<TgaImageRGB>(header, data, footer);
 
     case TgaImageType::GRAYSCALE:
     case TgaImageType::RLE_GRAYSCALE:
         if(verify_header<TgaImageGrayscale>(header)) {
             return std::unexpected(TgaImageError::INVALID_HEADER);
         }
-        return std::make_unique<TgaImageGrayscale>(header, data);
+        return std::make_unique<TgaImageGrayscale>(header, data, footer);
 
     default: return std::unexpected(TgaImageError::BAD_TYPE);
     }
